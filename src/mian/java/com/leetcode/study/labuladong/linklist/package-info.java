e3d0d5ba7197ea0b899910daf09b0a7d23d513b1
tenghu <th134757@163.com>
2023-06-10 23:41:38 -0700

    merge order list

---
+++ src/mian/java/com/leetcode/study/labuladong/linklist/package-info.java
@@ -0,0 +1,5 @@
+package com.leetcode.study.labuladong.linklist;
+
+/**
+ * Step-by-step guid to mastering algorithms related to linked list
+ */
---
+++ src/mian/java/com/leetcode/study/labuladong/package-info.java
@@ -0,0 +1,5 @@
+package com.leetcode.study.labuladong;
+
+/**
+ * Learning algorithms from the great god tabulation
+ */
