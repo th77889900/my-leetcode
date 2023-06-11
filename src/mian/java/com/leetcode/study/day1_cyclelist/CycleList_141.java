95159bc3af4b5ea5d1fd48547981bdab590a502d
tenghu <th134757@163.com>
2023-06-10 22:04:27 -0700

    code new

---
+++ src/mian/java/com/leetcode/study/day1_cyclelist/CycleList_141.java
@@ -0,0 +1,34 @@
+package com.leetcode.study.day1_cyclelist;
+
+/**
+ * Determine whether there exists a cycle in a linked list.
+ * @author : tenghu
+ * @date : 2023-04-30 5:13 p.m.
+ */
+public class CycleList_141 {
+
+    public static void main(String[] args) {
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static Boolean hasCycle(ListNode head) {
+
+
+
+
+        return false;
+    }
+
+
+
+}
---
+++ src/mian/java/com/leetcode/study/day1_cyclelist/package-info.java
@@ -0,0 +1,3 @@
+package com.leetcode.study.day1_cyclelist;
+
+// leet code 141 and 142 the problem of cycle list
